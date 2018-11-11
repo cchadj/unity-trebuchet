92513b8dfd97dde9436d1a432dca66b3d8df734d
Chadjiminas Chrysostomos <cchadj01@cs.ucy.ac.cy>
2018-11-12 01:55:02 +0200

    on space release

---
+++ Assets/ReleaseOnEnter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HingeJoint))]
+public class ReleaseOnEnter : MonoBehaviour {
+
+    public HingeJoint restrainHingeJoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            restrainHingeJoint.breakForce = 0;
+        }
+	}
+}
