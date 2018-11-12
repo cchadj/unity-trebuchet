c348726e20f1665159709da0731663f00a08af20
Chadjiminas Chrysostomos <cchadj01@cs.ucy.ac.cy>
2018-11-12 16:25:18 +0200

    push

--- Assets/Reload.cs
+++ Assets/Reload.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Reload : MonoBehaviour {
 
+    public Rigidbody rb;
+    public GameObject boulderPrefab;
+    public Transform sling;
+
+    public HingeJoint hj;
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.R))
+		if(Input.GetButton("Fire2"))
         {
-            
+            rb.mass = 15000;
+            GameObject.Instantiate(boulderPrefab, sling, false);
         }
 	}
 }
